9bb6adeb27e6c29d2abd80e65ad424c511aa658e
Aleksei Raguzin <a.raguzin83@gmail.com>
2023-11-17 21:57:10 +0300

    add lesson14

---
+++ infra/terraform/lesson14/main.tf
@@ -0,0 +1,58 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami             = data.aws_ami.ubuntu.id
+  key_name        = var.key_name
+  instance_type   = "t2.micro"
+  user_data       = file("init.sh")
+  security_groups = [aws_security_group.allow_http.name]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+data "aws_vpc" "selected" {
+  default = true
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "http"
+  }
+}
